2b8a89877c863b52fb96f170e0801f917cc0128a
Ericson "Fogo" Soares <ericson.ds999@gmail.com>
2018-02-12 19:54:54 -0200

    Initial boilerplate project architecture

---
+++ src/dictawav/lib.rs
@@ -0,0 +1,9 @@
+pub mod wav_handler;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+extern crate dictawav;
+
+fn main(){
+    println!("Hello World!");
+}
